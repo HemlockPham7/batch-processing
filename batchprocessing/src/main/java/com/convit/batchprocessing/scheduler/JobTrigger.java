d4c3e623b16f4870735c3e2444337b7edbe2bd1f
Viet-Hoang Pham <157110579+HemlockPham7@users.noreply.github.com>
2025-12-24 06:28:52 +0700

    Batch processing for json

--- batchprocessing/src/main/java/com/convit/batchprocessing/scheduler/JobTrigger.java
+++ batchprocessing/src/main/java/com/convit/batchprocessing/scheduler/JobTrigger.java
@@ -1,24 +1,31 @@
 package com.convit.batchprocessing.scheduler;
 
-import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
 @Component
-@RequiredArgsConstructor
 @Slf4j
 public class JobTrigger {
 
     private final JobLauncher jobLauncher;
     private final Job job;
+
+    public JobTrigger(
+            JobLauncher jobLauncher,
+            @Qualifier("importVehicleJsonJob") Job job
+    ) {
+        this.jobLauncher = jobLauncher;
+        this.job = job;
+    }
 
     @Scheduled(cron = "0/30 * * ? * *")
     @SneakyThrows
