c5ed7d1e5456f7f16a4bf1c68b3b37a4dbda1660
Viet-Hoang Pham <157110579+HemlockPham7@users.noreply.github.com>
2025-12-25 15:42:31 +0700

    Multi part upload to s3

--- batchprocessing/src/main/java/com/convit/batchprocessing/service/impl/StoreFileToObjectStorageServiceImpl.java
+++ batchprocessing/src/main/java/com/convit/batchprocessing/service/impl/StoreFileToObjectStorageServiceImpl.java
@@ -1,8 +1,5 @@
 package com.convit.batchprocessing.service.impl;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectResult;
 import com.convit.batchprocessing.service.StoreFileToObjectStorageUseCase;
 import com.convit.batchprocessing.service.dto.ObjectDTO;
 import lombok.RequiredArgsConstructor;
@@ -10,17 +7,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
+import software.amazon.awssdk.transfer.s3.S3TransferManager;
+import software.amazon.awssdk.transfer.s3.model.CompletedFileUpload;
+import software.amazon.awssdk.transfer.s3.model.FileUpload;
+import software.amazon.awssdk.transfer.s3.model.UploadFileRequest;
 
-import java.io.ByteArrayInputStream;
-import java.time.LocalDateTime;
-import java.util.Objects;
+import java.nio.file.Paths;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class StoreFileToObjectStorageServiceImpl implements StoreFileToObjectStorageUseCase {
 
-    private final AmazonS3 amazonS3;
+
+    private final S3TransferManager s3TransferManager;
 
     @Value("${cos.aws.s3.bucket.name}")
     private String bucket;
@@ -29,30 +29,25 @@ public class StoreFileToObjectStorageServiceImpl implements StoreFileToObjectSto
     public String store(ObjectDTO objectDTO) {
         createBucketIfDoesNotExist(bucket);
 
-        ObjectMetadata objectMetadata = mapOjectDtoToS3ObjectMetadata(objectDTO);
-        PutObjectResult putObjectResult = amazonS3.putObject(bucket, objectDTO.name(),
-                new ByteArrayInputStream(objectDTO.data()), objectMetadata);
-        log.info("The object '{}' has been stored. The metadata short info is {}", objectDTO.name(), putObjectResult);
+        UploadFileRequest uploadFileRequest = UploadFileRequest.builder()
+                .putObjectRequest(builder -> builder.bucket(bucket)
+                        .key(objectDTO.name()))
+                .source(Paths.get(objectDTO.name()))
+                .build();
+
+        FileUpload fileUpload = s3TransferManager.uploadFile(uploadFileRequest);
+
+        CompletedFileUpload completedFileUpload = fileUpload.completionFuture().join();
+        boolean successful = completedFileUpload.response().sdkHttpResponse().isSuccessful();
+
+        log.info("-----------> upload succeeded: {}", successful);
+
         return objectDTO.name();
     }
 
     @Override
     public void createBucketIfDoesNotExist(final String bucketName) {
         Assert.hasText(bucketName, "The bucket name should not be blank");
-        if (!amazonS3.doesBucketExistV2(bucketName)) {
-            amazonS3.createBucket(bucketName);
-        }
-    }
-
-    public ObjectMetadata mapOjectDtoToS3ObjectMetadata(final ObjectDTO objectDTO) {
-        Objects.requireNonNull(objectDTO, "the object dto should not be null");
-
-        final var objectMetadata = new ObjectMetadata();
-        objectMetadata.addUserMetadata("filename", objectDTO.name());
-        objectMetadata.addUserMetadata("storeDate", LocalDateTime.now().toString());
-        objectMetadata.setContentType(objectDTO.contentType());
-        objectMetadata.setContentLength(objectDTO.data().length);
-        return objectMetadata;
     }
 
 }
